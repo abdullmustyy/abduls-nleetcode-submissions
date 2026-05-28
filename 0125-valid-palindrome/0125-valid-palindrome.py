39722b59321629cc50491f2f0deecfb5b5dbb0ab
Abdul <abdulmusty03@outlook.com>
2026-05-28 02:01:50 +0100

    Time: 74 ms (5.14%), Space: 23.4 MB (12.97%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isalphanum(self, c: str) -> bool:
+        return (
+            ord("A") <= ord(c) <= ord("Z")
+            or ord("a") <= ord(c) <= ord("z")
+            or ord("0") <= ord(c) <= ord("9")
+        )
+
+    def isPalindrome(self, s: str) -> bool:
+        s = "".join(c.lower() for c in s if self.isalphanum(c))
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            if s[l] != s[r]:
+                return False
+
+            l += 1
+            r -= 1
+
+        return True
