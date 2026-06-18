e3cf9ec15338f96dd187b9347974d98eaa9c8856
Abdul <abdulmusty03@outlook.com>
2026-06-18 16:53:01 +0100

    Time: 435 ms (97.72%), Space: 22.1 MB (93.9%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -29,17 +29,3 @@ class Solution:
                         l += 1
 
         return res
-
-"""
-two pointers:
-- initialise a res list to store the result
-- sort the nums list
-- iterate over enumerated nums, for i, and n, initialise
-left (l) and right (r) pointers to the next and last element
-respectively
-- while l < r, add n, mums[l] and nums[r] -> threeSum
-- if threeSum greater than 0, decrease r, if less, increase l,
-if equal append [n, mums[l], nums[r]] to res
-- return res
-
-"""
